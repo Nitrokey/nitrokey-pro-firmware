051ce2a51fe8dc37460bb0733bf2bc6af36e5ed6
ggkitsas <ggkitsas@yahoo.com>
2014-10-22 17:43:03 +0200

    defined pwd-safe flash area

--- src/INC/password_safe.h
+++ src/INC/password_safe.h
@@ -36,8 +36,8 @@ void IBN_PWS_Tests (unsigned char nParamsGet_u8,unsigned char CMD_u8,unsigned in
 
 
 #define FLASH_START             0x80000000
-#define PWS_FLASH_START_PAGE    510
-#define PWS_FLASH_START_ADDRESS (0x80000000 + PWS_FLASH_START_PAGE * FLASH_PAGE_SIZE + (512*0)) // 0x8003FC00
+#define PWS_FLASH_START_PAGE    4096
+#define PWS_FLASH_START_ADDRESS (0x80000000 + PWS_FLASH_START_PAGE * FLASH_PAGE_SIZE + (512*0)) // 0x80200000
 
 
 #define PWS_SLOT_COUNT            16
