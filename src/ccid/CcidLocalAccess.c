39f750cae9f61effaa59514cbd965880070770a5
Szczepan Zalega <szczepan@nitrokey.com>
2018-04-30 23:03:21 +0200

    Set USB serial to HSM smartcard's one
    Select proper SC application and extract the serial.
    
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/ccid/CcidLocalAccess.c
+++ src/ccid/CcidLocalAccess.c
@@ -196,10 +196,8 @@ unsigned short SendAPDU (typeSmartcardTransfer * _tSCT)
 
     SendTPDU (_tSCT);
 
-    if (APDU_ANSWER_RECEIVE_INCORRECT == _tSCT->cAPDUAnswerStatus)   // return
-        // on
-        // orror
-        // ??
+    if (APDU_ANSWER_RECEIVE_INCORRECT == _tSCT->cAPDUAnswerStatus
+        && APDU_EOF != tSCT->cAPDUAnswerStatus)
     {
         return (_tSCT->cAPDUAnswerStatus);
     }
@@ -210,10 +208,7 @@ unsigned short SendAPDU (typeSmartcardTransfer * _tSCT)
         GenerateChainedTPDU (_tSCT);
         SendTPDU (_tSCT);
 
-        if ((APDU_ANSWER_CHAINED_DATA != _tSCT->cAPDUAnswerStatus) &&    // return
-            // on
-            // error
-            // ??
+        if ((APDU_ANSWER_CHAINED_DATA != _tSCT->cAPDUAnswerStatus) &&
             (APDU_ANSWER_COMMAND_CORRECT != _tSCT->cAPDUAnswerStatus))
         {
             return (_tSCT->cAPDUAnswerStatus);
@@ -858,6 +853,61 @@ int getAID (void)
 
     return nReturnSize;
 }
+
+uint32_t min(const uint32_t a, const uint32_t b){
+  return a>b ? b : a;
+}
+
+uint16_t send_data_no_response(uint8_t *data, uint8_t data_length){
+  uint16_t cRet;
+  tSCT.cAPDULength = data_length;
+  memcpy ((void *) &tSCT.cAPDU, data, tSCT.cAPDULength);
+  cRet = SendAPDU (&tSCT);
+  return  cRet;
+}
+
+retCode getSerialNumber (uint8_t * out_serial_number, const uint16_t buffer_length)
+{
+  InitSCTStruct (&tSCT);
+  retCode cRet = {};
+
+  //select HSM App
+  unsigned char cSelectHSM1[] = { 0x00, 0xA4, 0x04, 0x00, 0x0B, 0xE8, 0x2B, 0x06, 0x01, 0x04, 0x01, 0x81, 0xC3, 0x1F, 0x02, 0x01 };
+  tSCT.cAPDULength = sizeof(cSelectHSM1);
+  memcpy ((void *) &tSCT.cAPDU, cSelectHSM1, tSCT.cAPDULength);
+  cRet.smartcard_ret = SendAPDU (&tSCT);
+  cRet.execution_phase++;
+  if (APDU_ANSWER_COMMAND_CORRECT != cRet.smartcard_ret) return  cRet;
+
+  //select HSM App
+  unsigned char cSelectHSM[] = { 0x00, 0xA4, 0x00, 0x00, 0x02, 0x2F, 0x02, 0x00 };
+  tSCT.cAPDULength = sizeof(cSelectHSM);
+  memcpy ((void *) &tSCT.cAPDU, cSelectHSM, tSCT.cAPDULength);
+  cRet.smartcard_ret = SendAPDU (&tSCT);
+  cRet.execution_phase++;
+  if (APDU_ANSWER_COMMAND_CORRECT != cRet.smartcard_ret) return cRet;
+
+
+  unsigned char getSerial[] = { 0x00, 0xB1, 0x00, 0x00, 0x00, 0x00, 0x04, 0x54, 0x02, 0x00, 0x00, 0x04, 0x00 };
+  tSCT.cAPDULength = sizeof(getSerial);
+  memcpy ((void *) &tSCT.cAPDU, getSerial, tSCT.cAPDULength);
+  cRet.smartcard_ret = SendAPDU (&tSCT);
+  cRet.execution_phase++;
+  cRet.len = tSCT.cAPDUAnswerLength;
+  if (APDU_ANSWER_COMMAND_CORRECT != cRet.smartcard_ret && APDU_EOF != cRet.smartcard_ret) return cRet;
+
+  const uint16_t SERIAL_OFFSET = 0x70;
+  const uint16_t SERIAL_LENGTH = 11;
+  if (buffer_length>SERIAL_LENGTH){
+    memset(out_serial_number, 0, buffer_length);
+    memcpy(out_serial_number, (void *) tSCT.cAPDU+SERIAL_OFFSET, min(buffer_length, SERIAL_LENGTH));
+  }
+  InitSCTStruct (&tSCT);
+
+  cRet.execution_phase++;
+  return cRet;
+}
+
 
 uint8_t getByteOfData (uint8_t x)
 {
