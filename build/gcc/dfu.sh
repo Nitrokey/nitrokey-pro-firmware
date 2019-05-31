d54e97d37c8930763a5dbfd970f8cc9cc0deea8e
Szczepan Zalega <szczepan@nitrokey.com>
2019-05-31 21:41:13 +0200

    DEBUG add DFU flashing script
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

---
+++ build/gcc/dfu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+STM32_Programmer_CLI -c port=swd -e all -w  ../../dapboot/build/dapboot-nkpro.bin  0x8000000 -v -rst
+arm-none-eabi-objcopy -O binary nitrokey-pro-firmware.elf nitrokey-pro-firmware.bin 
+sudo dfu-util -D nitrokey-pro-firmware.bin
