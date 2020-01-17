af31c0491f51dd19199731b45b727a00015a9b35
Szczepan Zalega <szczepan@nitrokey.com>
2020-01-17 18:03:22 +0100

    Update notes and build script

--- build/gcc/dfu.sh
+++ build/gcc/dfu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-STM32_Programmer_CLI -c port=swd -e all -w  ../../dapboot/build/dapboot-nkpro.bin  0x8000000 -v -rst
+STM32_Programmer_CLI -c port=swd -e all -w  ../../../dapboot/build/dapboot-nkpro.bin  0x8000000 -v -rst
+sleep 1
 arm-none-eabi-objcopy -O binary nitrokey-pro-firmware.elf nitrokey-pro-firmware.bin 
 sudo dfu-util -D nitrokey-pro-firmware.bin
