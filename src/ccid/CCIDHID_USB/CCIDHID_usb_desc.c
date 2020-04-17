cd098a0e7a50950a881df973245a974e244a88e4
Cyril Lugan <cyril@lugan.fr>
2020-04-17 11:20:16 +0200

    Cleanup USB descriptor comments
    - Kept the descriptor content as-is, checked comments agains libusb
    output
    - Replaced some defined values with real one to be easier to compare
      with specs and other implementations
    - Aligned comments

--- src/ccid/CCIDHID_USB/CCIDHID_usb_desc.c
+++ src/ccid/CCIDHID_USB/CCIDHID_usb_desc.c
@@ -25,28 +25,27 @@
 #define WBVAL(x) (x&0xff),((x>>8)&0xff)
 
 const uint8_t CCID_DeviceDescriptor[CCID_SIZ_DEVICE_DESC] = {
-    0x12,   /* DeviceDescriptor bLength */
-    0x01,   /* bDescriptorType */
-    WBVAL (0x0110), /* bcdUSB, version 1.10 */
-    0x00,   /* bDeviceClass : each interface define the device class */
-    0x00,   /* bDeviceSubClass */
-    0x00,   /* bDeviceProtocol */
-    0x40,   /* bMaxPacketSize0 0x08 - 0x40 = 64 */
+    0x12,           // bLength           Descriptor size (18 bytes)
+    0x01,           // bDescriptorType   Descriptor type (device)
+    WBVAL (0x0110), // bcdUSB            USB version 1.1
+    0x00,           // bDeviceClass      Device class (specified at interface)
+    0x00,           // bDeviceSubClass
+    0x00,           // bDeviceProtocol
+    0x40,           // bMaxPacketSize0   Maximum packet size (64)
 
-    WBVAL (GLOBAL_VID), /* VID for CCID driver idVendor */
-    WBVAL (GLOBAL_PID), /* PID for CCID driver idProduct */
-    // WBVAL(0x0800), /* Test VID for MS CCID driver idVendor */
-    // WBVAL(0x0006), /* Test PID for MS CCID driver idProduct */
-    // WBVAL(0x08e6), /* 0x08e6 = idVendor germalto - for testing */
-    // WBVAL(0x3437), /* 0x3437 = idProduct germalto usb-sl reader - for
-    // testing */
+    WBVAL (GLOBAL_VID), // idVendor      VID
+    WBVAL (GLOBAL_PID), // idProduct     PID
+    // WBVAL(0x0800), // Test VID for MS CCID driver idVendor
+    // WBVAL(0x0006), // Test PID for MS CCID driver idProduct
+    // WBVAL(0x08e6), // 0x08e6 = idVendor germalto - for testing
+    // WBVAL(0x3437), // 0x3437 = idProduct germalto usb-sl reader - for
 
-    WBVAL (0x0101), /* bcdDevice version 1.01 */
+    WBVAL (0x0100), // bcdDevice version 1.00
 
-    1,  /* index of string Manufacturer */
-    2,  /* index of string descriptor of product */
-    3,  // 3, RB /* index of serial number */
-    0x01    /* bNumConfigurations */
+    0x01,  // iManufacturer       Index of string Manufacturer
+    0x02,  // iProduct            Index of string descriptor of product
+    0x03,  // iSerialNumber       Index of serial number
+    0x01   // bNumConfigurations  Number of configurations
 };
 
 /*****************************************************************************/
@@ -56,80 +55,83 @@ const uint8_t CCID_DeviceDescriptor[CCID_SIZ_DEVICE_DESC] = {
 
 const uint8_t CCID_ConfigDescriptor[CCID_SIZ_CONFIG_DESC] = {
 
-    // Configuration 1 Descriptor
-    0x09,   /* bLength ConfigDescriptor */
-    0x02,   /* bDescriptorType */
-    CCID_SIZ_CONFIG_DESC,
-    0x00,
-    0x02,   /* bNumInterfaces */
-    0x01,   /* bConfigurationValue */
-    0x00,   /* iConfiguration CCID = 6 ???? */
-    USB_CONFIG_BUS_POWERED, /* bmAttributes */
-    100 / 2,    /* MaxPower 200 mA */
+    // Configuration descriptor
+
+    0x09,     // bLength              Descriptor size (9 bytes)
+    0x02,     // bDescriptorType      Descriptor type (configuration)
+    WBVAL (CCID_SIZ_CONFIG_DESC),
+              // wTotalLength         Length of this + subordinate descriptors
+    0x02,     // bNumInterfaces       Number of interfaces
+    0x01,     // bConfigurationValue  Index of this configuration
+    0x00,     // iConfiguration       Configuration string (none)
+    0x80,     // bmAttributes         Bus powered
+    100 / 2,  // bMaxPower            100 mA
 
     // Interface 0 descriptor (Generic HID)
-    0x09,   /* bLength */
-    USB_INTERFACE_DESCRIPTOR_TYPE,  /* bDescriptorType */
-    0x00,   /* bInterfaceNumber */
-    0x00,   /* bAlternateSetting */
-    0x01,   /* bNumEndpoints = 1 */
-    0x03,   /* bInterfaceClass: HID */
-    0x00,   /* bInterfaceSubClass : 1=BOOT, 0=no boot */
-    0x00,   /* nInterfaceProtocol : 0=none, 1=keyboard, 2=mouse */
-    0,  /* iInterface: Index of string descriptor */
 
-    // Generic HID Descriptor
-    0x09,   /* bLength: HID Descriptor size */
-    HID_DESCRIPTOR_TYPE,    /* bDescriptorType: HID */
-    0x10,   /* bcdHID: HID Class Spec release number */
-    0x01,
-    0x00,   /* bCountryCode: Hardware target country */
-    0x01,   /* bNumDescriptors: Number of HID class descriptors to follow */
-    0x22,   /* bDescriptorType */
-    KEYBOARD_SIZ_REPORT_DESC,   /* wItemLength: Total length of Report descriptor */
-    0x00,
+    0x09, // bLength               Descriptor size (9 bytes)
+    0x04, // bDescriptorType       Descriptor type (interface)
+    0x00, // bInterfaceNumber      Interface Number
+    0x00, // bAlternateSetting     Alternate Setting Number (none)
+    0x01, // bNumEndpoints         Number of endpoints
+    0x03, // bInterfaceClass       Interface class (HID)
+    0x00, // bInterfaceSubClass    Interface subclass
+    0x00, // nInterfaceProtocol    Interface protocol
+    0x00, // iInterface            Interface string index (none)
 
-    // Endpoint 4 descriptor (Interrupt in)
-    0x07,   /* bLength */
-    0x05,   // bDescriptorType: Endpoint descriptor type
-    0x84,   // bEndpointAddress: Endpoint 4 IN
-    0x03,   // bmAttributes: Interrupt endpoint
-    0x08,   // wMaxPacketSize(LSB): 8 char max (0x0008)
-    0x00,   // wMaxPacketSize (MSB)
-    0x0A,
+    // Generic HID class descriptor
+
+    0x09,       // bLength           Descriptor size (9 bytes)
+    0x21,       // bDescriptorType   Descriptor type (HID)
+    0x10, 0x01, // bcdHID            HID release number (1.1)
+    0x00,       // bCountryCode      Hardware target country (not supported)
+    0x01,       // bNumDescriptors   Number class descriptors
+    0x22,       // bDescriptorType   Class descriptor type (report)
+    WBVAL (KEYBOARD_SIZ_REPORT_DESC),
+                // wItemLength       Report descriptor size
+
+    // Interrupt IN endpoint 4 descriptor
+
+    0x07,       // bLength           Descriptor size (7 bytes)
+    0x05,       // bDescriptorType   Descriptor type (endpoint)
+    0x84,       // bEndpointAddress  Endpoint 4 IN
+    0x03,       // bmAttributes      Transfer type (interrupt)
+    0x08, 0x00, // wMaxPacketSize    Maximum packet size (8 bytes)
+    0x0A,       // bInterval
 
     // Interface 1 descriptor (Smart Card Reader)
-    0x09,   /* bLength */
-    0x04,   /* bDescriptorType */
-    0x01,   /* bInterfaceNumber */
-    0x00,   /* bAlternateSetting */
-    0x03,   /* bNumEndpoints = 3 */
-    0x0B,   /* bInterfaceClass = CCID */
-    0x00,   /* bInterfaceSubClass */
-    0x00,   /* bInterfaceProtocol */
-    0x00,   /* 0x04, = 0 ??? iInterface Index of string descriptor (befor 0x64) */
+
+    0x09,   // bLength               Descriptor size (9 bytes)
+    0x04,   // bDescriptorType       Descriptor type (interface)
+    0x01,   // bInterfaceNumber      Interface Number
+    0x00,   // bAlternateSetting     Alternate Setting Number (none)
+    0x03,   // bNumEndpoints         Number of endpoints
+    0x0B,   // bInterfaceClass       Interface class (CCID)
+    0x00,   // bInterfaceSubClass    Interface subclass
+    0x00,   // nInterfaceProtocol    Interface protocol
+    0x00,   // iInterface            Interface string index (none)
 
     // CCID class descriptor
-    0x36,   // bLength: CCID Descriptor size
-    0x21,   // bDescriptorType: HID To be updated with CCID specific number
-    0x10,   // bcdHID(LSB): CCID Class Spec release number (1.10)
-    0x01,   // bcdHID(MSB)
 
-    0x00,   // bMaxSlotIndex
-    0x02,   // bVoltageSupport: 3v
+    0x36,       // bLength          CCID Descriptor size
+    0x21,       // bDescriptorType  HID To be updated with CCID specific number
+    0x10, 0x01, // bcdHID           CCID Class Spec release number (1.10)
 
-    0x02, 0x00, 0x00, 0x00, // dwProtocols: supports T=1
-    0x10, 0x0E, 0x00, 0x00, // dwDefaultClock: 3,6 Mhz (0x00000E10)
-    0x10, 0x0E, 0x00, 0x00, // dwMaximumClock: 3,6 Mhz (0x00000E10)
-    0x00,   // bNumClockSupported => only default clock
+    0x00,       // bMaxSlotIndex
+    0x02,       // bVoltageSupport  3v
 
-    0xCD, 0x25, 0x00, 0x00, // dwDataRate: 9677 bps (0x000025CD)
-    0xA1, 0xC5, 0x01, 0x00, // dwMaxDataRate: 116129 bps (0x0001C5A1)
-    0x00,   // bNumDataRatesSupported => no manual setting
+    0x02, 0x00, 0x00, 0x00, // dwProtocols              supports T=1
+    0x10, 0x0E, 0x00, 0x00, // dwDefaultClock           3,6 Mhz (0x00000E10)
+    0x10, 0x0E, 0x00, 0x00, // dwMaximumClock           3,6 Mhz (0x00000E10)
+    0x00,                   // bNumClockSupported       only default clock
 
-    0xFE, 0x00, 0x00, 0x00, /* dwMaxIFSD: 254 */
-    0x00, 0x00, 0x00, 0x00, /* dwSynchProtocols */
-    0x00, 0x00, 0x00, 0x00, /* dwMechanical: no special characteristics */
+    0xCD, 0x25, 0x00, 0x00, // dwDataRate               9677 bps (0x000025CD)
+    0xA1, 0xC5, 0x01, 0x00, // dwMaxDataRate            116129 bps (0x0001C5A1)
+    0x00,                   // bNumDataRatesSupported   no manual setting
+
+    0xFE, 0x00, 0x00, 0x00, // dwMaxIFSD                254
+    0x00, 0x00, 0x00, 0x00, // dwSynchProtocols
+    0x00, 0x00, 0x00, 0x00, // dwMechanical             no special characteristics
 
     // 0x3E,0x00,0x02,0x00, // instand pc reset on XP :-)
     0xBA, 0x04, 0x01, 0x00, // 000104BAh
@@ -141,53 +143,49 @@ const uint8_t CCID_ConfigDescriptor[CCID_SIZ_CONFIG_DESC] = {
     // 00000400h Automatic IFSD exchange
     // 00010000h TPDU level exchanges with CCID
 
-    // 0x24,0x00,0x00,0x00, /* dwMaxCCIDMessageLength : Maximun block size +
-    // header*/
-    0x0F, 0x01, 0x00, 0x00, /* dwMaxCCIDMessageLength : Maximun block size + header */
-    /* 261 + 10 */
+    0x0F, 0x01, 0x00, 0x00, // dwMaxCCIDMessageLength  Maximun block size + header (261 + 10)
 
-    0x00,   /* bClassGetResponse */
-    0x00,   /* bClassEnvelope */
-    0x00, 0x00, /* wLcdLayout */
-    0x00,   /* bPINSupport : no PIN verif and modif */
-    0x01,   /* bMaxCCIDBusySlots */
+    0x00,       // bClassGetResponse
+    0x00,       // bClassEnvelope
+    0x00, 0x00, // wLcdLayout
+    0x00,       // bPINSupport        no PIN verif and modif
+    0x01,       // bMaxCCIDBusySlots
 
-    // Endpoint 1 descriptor (Interrupt in SCR)
-    0x07,   /* bLength */
-    0x05,   // bDescriptorType: Endpoint descriptor type
-    0x81,   // bEndpointAddress: Endpoint 1 IN
-    0x03,   // bmAttributes: Interrupt endpoint
-    0x40,   // 0x08, zur sicherheit 0x40 // wMaxPacketSize(LSB): 8 char max
-    // (0x0008)
-    0x00,   // wMaxPacketSize (MSB)
-    0x18,   // bInterval: Polling Interval (24 ms = 0x18)
+    // Interrupt OUT endpoint 1 descriptor (SCR)
 
-    // Endpoint 2 descriptor (Bulk out SCR)
-    0x07,   /* bLength */
-    0x05,   // bDescriptorType: Endpoint descriptor type
-    0x02,   // bEndpointAddress: Endpoint 2 OUT
-    0x02,   // bmAttributes: Bulk endpoint
-    0x40,   // wMaxPacketSize(LSB): 64 char max (0x0040)
-    0x00,   // wMaxPacketSize (MSB)
-    0x00,   // bInterval: ignored
+    0x07,       // bLength           Descriptor size (7 bytes)
+    0x05,       // bDescriptorType   Descriptor type (endpoint)
+    0x81,       // bEndpointAddress  Endpoint 1 IN
+    0x03,       // bmAttributes      Transfer type (interrupt)
+    0x40, 0x00, // wMaxPacketSize    Maximum packet size (TODO 0x08, zur sicherheit 0x40)
+    0x18,       // bInterval         Polling interval (24 ms = 0x18)
 
-    // Endpoint 2 descriptor (Bulk in SCR)
-    0x07,   /* bLength */
-    0x05,   // bDescriptorType: Endpoint descriptor type
-    0x82,   // RB to avoid doublebuffering ? 0x82,// bEndpointAddress:
-    0x02,   // bmAttributes: Bulk endpoint
-    0x40,   // wMaxPacketSize(LSB): 64 char max (0x0040)
-    0x00,   // wMaxPacketSize (MSB)
-    0x00,   // bInterval: ignored
+    // Bulk OUT endpoint 1 descriptor (SCR)
+
+    0x07,       // bLength           Descriptor size (7 bytes)
+    0x05,       // bDescriptorType   Descriptor type (endpoint)
+    0x02,       // bEndpointAddress  Endpoint 2 OUT
+    0x02,       // bmAttributes      Transfer type (bulk)
+    0x40, 0x00, // wMaxPacketSize    Maximum packet size (64 bytes)
+    0x00,       // bInterval         Polling interval (ignored)
+
+    // Bulk IN endpoint 2 descriptor (SCR)
+
+    0x07,       // bLength           Descriptor size (7 bytes)
+    0x05,       // bDescriptorType   Descriptor type (endpoint)
+    0x82,       // bEndpointAddress  Endpoint 2 IN
+    0x02,       // bmAttributes      Transfer type (bulk)
+    0x40, 0x00, // wMaxPacketSize    Maximum packet size (64 bytes)
+    0x00,       // bInterval         Polling interval (ignored)
 };
 
 
 const uint8_t Keyboard_ReportDescriptor[KEYBOARD_SIZ_REPORT_DESC] = {
-    0x06, 0x00, 0xff,	// USAGE_PAGE (Vendor-Defined)
-    0x09, 0x01,			// USAGE (Vendor)
-    0xa1, 0x01,			// COLLECTION (Application)
+    0x06, 0x00, 0xff,    // USAGE_PAGE (Vendor-Defined)
+    0x09, 0x01,          // USAGE (Vendor)
+    0xa1, 0x01,          // COLLECTION (Application)
 
-        0x09, 0x01,			//   USAGE (Input Report Data) !!! 0x0920
+        0x09, 0x01,         //   USAGE (Input Report Data) !!! 0x0920
         0x15, 0x00,         //   LOGICAL_MINIMUM (0)
         0x26, 0xff, 0x00,   //   LOGICAL_MAXIMUM (255)
         0x75, 0x08,         //   REPORT_SIZE (8)
@@ -200,13 +198,13 @@ const uint8_t Keyboard_ReportDescriptor[KEYBOARD_SIZ_REPORT_DESC] = {
         0x75, 0x08,         //   REPORT_SIZE (8)
         0x95, 0x40,         //   REPORT_COUNT (64)
 
-        0x91, 0x02, 		//   OUTPUT (Data,Var,Abs)
-        0x09, 0x02, 		//   USAGE (Keyboard ErrorUndefine)
-        0x75, 0x08, 		//   REPORT_SIZE (8)
+        0x91, 0x02,         //   OUTPUT (Data,Var,Abs)
+        0x09, 0x02,         //   USAGE (Keyboard ErrorUndefine)
+        0x75, 0x08,         //   REPORT_SIZE (8)
         0x95, KEYBOARD_FEATURE_COUNT,   // REPORT_COUNT (64)
-        0xB1, 0x02, 		//   FEATURE (Data,Var,Abs) */
+        0xB1, 0x02,         //   FEATURE (Data,Var,Abs) */
 
-    0xc0,   			// END_COLLECTION
+    0xc0,               // END_COLLECTION
 };
 
 /*****************************************************************************/
