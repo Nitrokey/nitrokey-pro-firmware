bd9150c5dbc0d1d946f0f57e0948999c72ae57bb
Szczepan Zalega <szczepan@nitrokey.com>
2021-01-04 17:54:14 +0100

    WIP: change delay and test

--- src/ccid/smartcard/smartcard.c
+++ src/ccid/smartcard/smartcard.c
@@ -1377,7 +1377,7 @@ int CRD_SendCommand (unsigned char* pTransmitBuffer, unsigned int nCommandSize, 
 #ifdef GERMALTO_CARD
     int i1;
 #endif
-    int nDelayTime;
+    u32 nDelayTime;
 
     int nStatus;
 
@@ -1434,21 +1434,13 @@ int CRD_SendCommand (unsigned char* pTransmitBuffer, unsigned int nCommandSize, 
 
     /* Get answer */
     //
-    for (i = 0; i < ICC_MESSAGE_BUFFER_MAX_LENGTH - USB_MESSAGE_HEADER_SIZE; i++)   // max
-        // buffer
-        // size
-        // (had
-        // to
-        // be
-        // checked)
+    for (i = 0; i < ICC_MESSAGE_BUFFER_MAX_LENGTH - USB_MESSAGE_HEADER_SIZE; i++)  // max buffer size (had to be checked)
     {
         nDelayTime = SC_Receive_Timeout;
-        if (0 == i)
-        {
-            nDelayTime = SC_Receive_Timeout * 10000L;   // Long long wait for
-            // first byte, allow
-            // card to work
-        }
+//        if (0 == i)
+//        {
+//            nDelayTime = SC_Receive_Timeout * 10000L;   // Long long wait for first byte, allow card to work
+//        }
 
         if ((USART_ByteReceive (&pTransmitBuffer[i], nDelayTime)) != SUCCESS)
         {
