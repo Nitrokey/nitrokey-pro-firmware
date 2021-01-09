0cf068b253cc98f24453f1d1374e5d34c08af083
Szczepan Zalega <szczepan@nitrokey.com>
2021-01-09 19:13:18 +0100

    Build: add Docker clean action

--- docker.mk
+++ docker.mk
@@ -1,4 +1,5 @@
 CMD=bash -c "cd /app && make clean && make"
+CMDCLEAN=bash -c "cd /app && make clean"
 
 all: build
 
@@ -10,3 +11,7 @@ dfu:
 build:
 	echo `git describe` > build/gcc/GIT_VERSION
 	sudo docker run -i --rm  -v ${PWD}:/app registry.git.dotplex.com/nitrokey/nitrokey-pro-firmware:compilation $(CMD)
+
+.PHONY: clean
+clean:
+	sudo docker run -i --rm  -v ${PWD}:/app registry.git.dotplex.com/nitrokey/nitrokey-pro-firmware:compilation $(CMDCLEAN)
