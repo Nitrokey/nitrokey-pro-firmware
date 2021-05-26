80fc9cfc15195bf9c7e926d3c4aaa404e2b492ca
Szczepan Zalega <szczepan@nitrokey.com>
2021-05-26 13:20:10 +0200

    SIGN Prepare to set the required pins to chosen state after boot

--- src/hw_config.c
+++ src/hw_config.c
@@ -20,6 +20,8 @@
  */
 
 /* Includes ------------------------------------------------------------------ */
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_rcc.h>
 #include "stm32f10x_it.h"
 #include "stm32f10x_systick.h"
 #include "hw_config.h"
@@ -52,6 +54,14 @@ void RCC_Config (void);
 
 /* Private functions --------------------------------------------------------- */
 
+enum Hardware {
+    HW_UNKNOWN,
+    HW_BGA,
+    HW_NON_BGA
+};
+enum Hardware detect_hardware(void);
+void init_BGA_hardware(void);
+uint32_t get_peripheral_for_port(GPIO_TypeDef *port);
 /*******************************************************************************
 
   DisableSmartcardLED
@@ -266,6 +276,10 @@ void Set_System (void)
     /* Enable and GPIOD clock */
     USB_Disconnect_Config ();
 
+    if (detect_hardware() == HW_BGA) {
+        init_BGA_hardware();
+    }
+
     init_blinking();
     EnableSmartcardLED ();
     EnableOATHLED ();
@@ -285,6 +299,47 @@ void Set_System (void)
     /* Enable SysTick Counter for time in firmware */
     SysTick_CounterCmd (SysTick_Counter_Enable);
 
+}
+
+// TODO "compress" the structure by using local ids instead of the full 32-bit GPIO pointers, e.g. map A => GPIOA separately
+struct PortPin {
+    GPIO_TypeDef * port;
+    uint16_t pin;
+};
+
+uint32_t get_peripheral_for_port(GPIO_TypeDef *port) {
+    if (port == GPIOA) return RCC_APB2Periph_GPIOA;
+    if (port == GPIOB) return RCC_APB2Periph_GPIOB;
+    if (port == GPIOC) return RCC_APB2Periph_GPIOC;
+    if (port == GPIOD) return RCC_APB2Periph_GPIOD;
+    // FIXME report error for invalid argument
+    return RCC_APB2Periph_GPIOA;
+}
+
+#define ARR_LEN(x)      (sizeof(x)/sizeof((x)[0]))
+void init_BGA_hardware(void) {
+
+    struct PortPin pins_to_input[] = {
+        {GPIOB, GPIO_Pin_7},
+        {GPIOB, GPIO_Pin_9},
+        {GPIOB, GPIO_Pin_13},
+        {GPIOC, GPIO_Pin_0},
+        {GPIOC, GPIO_Pin_5},
+        {GPIOC, GPIO_Pin_7},
+        {GPIOC, GPIO_Pin_8},
+        {GPIOC, GPIO_Pin_15},
+    };
+
+    for (int i = 0; i < ARR_LEN(pins_to_input); ++i) {
+        // enable port clock
+        GPIO_InitTypeDef GPIO_InitStructure;
+        RCC_APB2PeriphClockCmd (get_peripheral_for_port(pins_to_input[i].port), ENABLE);
+        GPIO_InitStructure.GPIO_Pin = pins_to_input->pin;
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+        // set pin modes
+        GPIO_Init ( pins_to_input[i].port, &GPIO_InitStructure);
+    }
 }
 
 /*******************************************************************************
@@ -504,6 +559,9 @@ void RCC_Config (void)
     /* Setup the microcontroller system. Initialize the Embedded Flash Interface, initialize the PLL and update the SystemFrequency variable. */
     SystemInit ();
 
+}
+enum Hardware detect_hardware(void) {
+    return HW_BGA;
 }
 
 /*******************************************************************************
