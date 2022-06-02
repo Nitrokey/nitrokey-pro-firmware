7fce21a1530aeb02e87e26debd7be61734cb2436
Szczepan Zalega <szczepan@nitrokey.com>
2022-06-02 15:59:40 +0200

    Disable asking for the smartcard serial number

--- src/ccid/CCIDHID_USB/CCIDHID_usb_prop.c
+++ src/ccid/CCIDHID_USB/CCIDHID_usb_prop.c
@@ -146,7 +146,7 @@ void USB_CCID_init (void)
 {
 
     /* Update the serial number string descriptor with the data from the unique ID */
-    Get_SerialNum ();
+//    Get_SerialNum ();
 
     pInformation->Current_Configuration = 0;
 
